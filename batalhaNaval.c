4e6b9fe4b6fbe441a5d869197a21c9f670f6939d
VitorCardoso14 <vitorcardoso1401@gmail.com>
2025-05-11 20:42:19 +0000

    adicionei a agua

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
 
-
+//Declarar a matriz sem ter que escrever numero por numero
+#define LINHAS 10
+#define COLUNAS 10
+#define AGUA 0
+#define NAVIO 3
 
 int main() {
+    
+
 //char para criar o array
 char linha[10] = {'A','B','C','D','E','F','G','H','I','J'};
+
 //matriz 10x10
-int i, j, Tabuleiro[10][10] = {
-{0,0,0,0,0,0,0,0,0,0},
-{0,0,0,0,0,0,0,0,0,0},
-{0,0,0,0,0,0,0,0,0,0},
-{0,0,0,0,0,0,0,0,0,0},
-{0,0,3,3,3,0,0,0,0,0},
-{0,0,0,0,0,0,0,0,0,0},
-{0,0,0,0,0,0,0,0,0,0},
-{0,0,0,0,0,0,0,0,0,0},
-{0,0,0,0,0,0,0,0,0,0},
-{0,0,0,0,0,0,0,0,0,0},
-};
+int i, j, k, Tabuleiro[LINHAS][COLUNAS], linhaNavil1 = 4, colunaNavil1 = 3, tamanhoNavil1 = 2;
+
 
 //comando para imprimir a linha
 printf("Tabuleiro Batalha Naval\n");
@@ -28,16 +25,20 @@ for ( i = 0; i < 10; i++)
 }
 printf("\n");
 
+
+
+
 //for para mostrar a matriz
-for (i = 0; i < 10; i++)
+for (i = 0; i < LINHAS; i++)
 {
     printf("%d ", i + 1);
-    for ( j = 0; j < 9; j++)
+    for ( j = 0; j < COLUNAS; j++)
     {
-        printf("%d ", Tabuleiro[i][j]);
+      printf("%d ", Tabuleiro[i][j] = AGUA);
     }
-    printf("%d\n", Tabuleiro[i][j]);
+    printf("\n");
 }
+
 
 
 
