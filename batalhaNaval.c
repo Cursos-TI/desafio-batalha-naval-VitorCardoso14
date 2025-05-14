38923d6ba90a34dd37cd3de3fdf61015bdcb30e8
VitorCardoso14 <vitorcardoso1401@gmail.com>
2025-05-14 22:26:06 +0000

    Fim

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -19,13 +19,27 @@ int linhaNavioDigD = 1;
 int colunaNavioDigD = 1;
 int linhaNavioDigE = 6;
 int colunaNavioDigE = 7;
-//habilidade
+//habilidade cone
 int habilidadeCone1 = 2;
 int habilidadeCone1a = 3;
 int habilidadeCone2 = 3;
 int habilidadeCone2a = 2;
 int habilidadeCone3 = 4;
 int habilidadeCone3a = 1;
+//habilidade cruz
+int habilidadeCruz1 = 1;
+int habilidadeCruz1a = 7;
+int habilidadeCruz2 = 2;
+int habilidadeCruz2a = 5;
+int habilidadeCruz3 = 3;
+int habilidadeCruz3a = 7;
+//habilidade octaedro
+int habilidadeOctaedro1 = 6;
+int habilidadeOctaedro1a = 5;
+int habilidadeOctaedro2 = 7;
+int habilidadeOctaedro2a = 4;
+int habilidadeOctaedro3 = 8;
+int habilidadeOctaedro3a = 5;
 
 //apenas para testar o conhecimento
 printf("Escolha a linha do navio horizontal: ");
@@ -74,13 +88,42 @@ for (int i = 0; i < 1; i++)
     Tabuleiro[habilidadeCone1][habilidadeCone1a] = 2;
 }
 for (int i = 0; i < 3; i++)
-{
-    Tabuleiro[habilidadeCone2][habilidadeCone2a + i] = 2;
+//if para nao passar dos limites do tabuleiro
+{if (habilidadeCone2a + i < COLUNAS){
+    if (habilidadeCone2 + i < LINHAS){
+    Tabuleiro[habilidadeCone2][habilidadeCone2a + i] = 2;}
+}
 }
 for (int i = 0; i < 5; i++)
-{
-    Tabuleiro[habilidadeCone3][habilidadeCone3a + i] = 2;
+//if para nao passar dos limites do tabuleiro
+{if (habilidadeCone3a + i < COLUNAS){
+    if(habilidadeCone3 + i < LINHAS){
+    Tabuleiro[habilidadeCone3][habilidadeCone3a + i] = 2;}
 }
+}
+
+//cruz
+    Tabuleiro[habilidadeCruz1][habilidadeCruz1a] = 2;
+
+for (int i = 0; i < 5; i++)
+//if para nao passar dos limites do tabuleiro
+{if (habilidadeCruz2a + i < COLUNAS){
+    if(habilidadeCruz2 + i < LINHAS){
+    Tabuleiro[habilidadeCruz2][habilidadeCruz2a + i] = 2;}
+}
+}
+    Tabuleiro[habilidadeCruz3][habilidadeCruz3a] = 2;
+
+//octaedro
+    Tabuleiro[habilidadeOctaedro1][habilidadeOctaedro1a] = 2;
+for (int i = 0; i < 3; i++)
+//if para nao passar dos limites do tabuleiro
+{if (habilidadeOctaedro2a + i < COLUNAS){
+    if(habilidadeOctaedro2 + i < LINHAS){
+    Tabuleiro[habilidadeOctaedro2][habilidadeOctaedro2a + i] = 2;}
+    }
+}
+    Tabuleiro[habilidadeOctaedro3][habilidadeOctaedro3a] = 2;
 
 //comando para imprimir a linha
 printf("Tabuleiro Batalha Naval\n");
